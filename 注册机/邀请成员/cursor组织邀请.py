2aa7a05e5fcc28570051aaa71400c047de0257d2
sineom <sineom@126.com>
2025-04-09 22:37:11 +0800

    补号

--- 注册机/邀请成员/cursor组织邀请.py
+++ 注册机/邀请成员/cursor组织邀请.py
@@ -2,7 +2,7 @@
 Author: sineom sineom@126.com
 Date: 2025-03-27 02:06:20
 LastEditors: sineom sineom@126.com
-LastEditTime: 2025-04-06 18:00:24
+LastEditTime: 2025-04-09 00:29:56
 FilePath: /邀请成员/cursor组织邀请.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -12,7 +12,7 @@ import json
 from tqdm import tqdm
 
 # 设置邀请代码
-invite_code = "ba658d8c7abdb85f063bddef9d35dae099de629d727a2102"
+invite_code = "3a8b6d85a42c1c5c66c433be03fc8df12b1bae18ebc4339a"
 
 # 读取cookies.txt文件
 with open('cookies.txt', 'r') as file:
