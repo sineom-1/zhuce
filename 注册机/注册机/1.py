02626e729e97a8d2635af32353a0f7aaf537625c
sineom <sineom@126.com>
2025-05-09 07:56:46 +0800

    新号

--- 注册机/注册机/1.py
+++ 注册机/注册机/1.py
@@ -260,7 +260,7 @@ def main():
 
 
 if __name__ == "__main__":
-    repeat_times = 5  # 注册次数
+    repeat_times = 9  # 注册次数
     success_count = 0
     
     for i in range(repeat_times):
--- 注册机/邀请成员/cursor组织邀请.py
+++ 注册机/邀请成员/cursor组织邀请.py
@@ -2,7 +2,7 @@
 Author: sineom sineom@126.com
 Date: 2025-03-27 02:06:20
 LastEditors: sineom sineom@126.com
-LastEditTime: 2025-05-02 07:24:30
+LastEditTime: 2025-05-06 06:13:51
 FilePath: /邀请成员/cursor组织邀请.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -15,7 +15,7 @@ import requests
 import concurrent.futures
 
 # 设置邀请代码
-invite_code = "6334508e3be906e5c1f6f4046b615752fda8077f5401eb19"
+invite_code = "355ffb41a5ad35f965a9293e32a143b36e75ff016532c562"
 
 # 读取cookies.txt文件
 with open('cookies.txt', 'r') as file:
